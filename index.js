82ed77a7308ce3eb90ab18b914e2627106958a1f
Christian Theilemann <christian@solvvy.com>
2018-12-10 01:53:18 +0100

    fix reading credentials from file by setting proper encoding
    without specifying the encoding readFileSync will return a Buffer instead of a string which crashes unleash

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const unleash = require('unleash-server');
 let options = {};
 
 if (process.env.DATABASE_URL_FILE) {
-    options.databaseUrl = fs.readFileSync(process.env.DATABASE_URL_FILE);
+    options.databaseUrl = fs.readFileSync(process.env.DATABASE_URL_FILE, {encoding: 'utf8'});
 }
 
 unleash.start(options);
