4fa5ed0991e7e0d33329ef0acefa5351f43e475a
ivaosthu <ivar.conradi.osthus@finn.no>
2019-06-18 20:17:24 +0200

    fix: update to unleash-server version 3.2.13
    closes #19

--- index.js
+++ index.js
@@ -5,8 +5,4 @@ const unleash = require('unleash-server');
 
 let options = {};
 
-if (process.env.DATABASE_URL_FILE) {
-    options.databaseUrl = fs.readFileSync(process.env.DATABASE_URL_FILE, 'utf8');
-}
-
 unleash.start(options);
