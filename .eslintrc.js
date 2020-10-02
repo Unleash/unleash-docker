07882e7ba8a1a9ca1315fbc1de360c40b5d8afbd
Eric Wooley <ericwooley@gmail.com>
2020-10-02 16:10:27 -0700

    add client protection

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+  globals: {
+    process: true,
+  },
 };
--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-'use strict'
-const path = require('path')
-const unleash = require('unleash-server')
-const passport = require('passport')
-const GoogleOAuth2Strategy = require('passport-google-oauth20').Strategy
-const { inspect } = require('util')
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || ''
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || ''
+"use strict";
+const unleash = require("unleash-server");
+const passport = require("passport");
+const GoogleOAuth2Strategy = require("passport-google-oauth20").Strategy;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "";
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || "";
 // should end with `'/api/auth/callback'` to match router
-const GOOGLE_CALLBACK_URL = process.env.GOOGLE_CALLBACK_URL || ''
-const sharedSecret = process.env.SHARED_SECRET || ''
-const allowedUsers = new RegExp(process.env.ALLOWED_USERS_REGEX)
-const enableGoogleLogin = !!GOOGLE_CLIENT_ID
+const GOOGLE_CALLBACK_URL = process.env.GOOGLE_CALLBACK_URL || "";
+const allowedUsers = new RegExp(process.env.ALLOWED_USERS_REGEX);
+const enableGoogleLogin = !!GOOGLE_CLIENT_ID;
 
 if (enableGoogleLogin) {
   passport.use(
@@ -19,7 +16,7 @@ if (enableGoogleLogin) {
         clientID: GOOGLE_CLIENT_ID,
         clientSecret: GOOGLE_CLIENT_SECRET,
         callbackURL: GOOGLE_CALLBACK_URL,
-        scope: 'email',
+        scope: "email",
       },
       (accessToken, refreshToken, profile, cb) => {
         cb(
@@ -28,82 +25,101 @@ if (enableGoogleLogin) {
             name: profile.displayName,
             email: profile.emails[0].value,
           })
-        )
+        );
       }
     )
-  )
+  );
 }
 
 function googleAdminAuth(app) {
-  app.use((req, res, next) => {
-    next()
-  })
-  if (!enableGoogleLogin) return
-  app.use(passport.initialize())
-  app.use(passport.session())
+  if (!enableGoogleLogin) return console.warn("Google login is not enabled");
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-  passport.serializeUser((user, done) => done(null, user))
-  passport.deserializeUser((user, done) => done(null, user))
+  passport.serializeUser((user, done) => done(null, user));
+  passport.deserializeUser((user, done) => done(null, user));
   app.get(
-    '/api/admin/login',
-    passport.authenticate('google', { scope: ['email'] })
-  )
+    "/api/admin/login",
+    passport.authenticate("google", { scope: ["email"] })
+  );
 
   app.get(
-    '/api/auth/callback',
-    passport.authenticate('google', {
-      successRedirect: '/',
-      failureRedirect: '/api/admin/error-login',
+    "/api/auth/callback",
+    passport.authenticate("google", {
+      successRedirect: "/",
+      failureRedirect: "/api/admin/error-login",
     })
-  )
+  );
 
-  app.get('/api/admin/error-login', (req, res) => {
+  app.get("/api/admin/error-login", (req, res) => {
     return res
-      .status('401')
+      .status("401")
       .json(
         new unleash.AuthenticationRequired({
-          path: '/api/admin/login',
-          type: 'custom',
+          path: "/api/admin/login",
+          type: "custom",
           message: `There was an error authenticating.`,
         })
       )
-      .end()
-  })
+      .end();
+  });
 
-  app.use('/api/admin/', (req, res, next) => {
-    let emailNotAllowedError = ''
-    let showInvalidEmailError = !!(req.user && req.user.email)
+  app.use("/api/admin/", (req, res, next) => {
+    let emailNotAllowedError = "";
+    let showInvalidEmailError = !!(req.user && req.user.email);
     if (req.user && req.user.email && req.user.email.match(allowedUsers)) {
-      return next()
+      return next();
     } else {
       emailNotAllowedError =
-        'Your email was not on the approved email list. Check with your administrator.'
+        "Your email was not on the approved email list. Check with your administrator.";
     }
     // Instruct unleash-frontend to pop-up auth dialog
     return res
-      .status('401')
+      .status("401")
       .json(
         new unleash.AuthenticationRequired({
-          path: '/api/admin/login',
-          type: 'custom',
+          path: "/api/admin/login",
+          type: "custom",
           message: showInvalidEmailError
             ? emailNotAllowedError
             : `You have to identify yourself in order to use Unleash.
                       Click the button and follow the instructions.`,
         })
       )
-      .end()
-  })
+      .end();
+  });
 }
 
-const options = enableGoogleLogin
-  ? {
-      enableLegacyRoutes: false,
-      adminAuthentication: 'custom',
-      preRouterHook: googleAdminAuth,
+const sharedSecret = process.env.SHARED_SECRET || "";
+function presharedClientAuth(app) {
+  if (!sharedSecret)
+    return console.warn("No shared secret, /api/client is insecure");
+  app.use("/api/client", (req, res, next) => {
+    if (req.header("authorization") !== sharedSecret) {
+      res.sendStatus(401);
+    } else {
+      next();
     }
-  : {}
+  });
+}
+
+const options =
+  enableGoogleLogin || sharedSecret
+    ? {
+        enableLegacyRoutes: false,
+        adminAuthentication: "custom",
+        preRouterHook: prestartAll(googleAdminAuth, presharedClientAuth),
+      }
+    : {};
 
 unleash.start(options).then((instance) => {
-  console.log(`Unleash started on http://localhost:${instance.app.get('port')}`)
-})
+  console.log(
+    `Unleash started on http://localhost:${instance.app.get("port")}`
+  );
+});
+
+function prestartAll(...callbacks) {
+  return (app) => {
+    callbacks.forEach((fn) => fn(app));
+  };
+}
